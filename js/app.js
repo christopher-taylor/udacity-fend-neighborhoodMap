125f209d9d9cab136fca6e9925780c5f4ac20245
LiquidSwordsman <ctaylor@liquidswords.ninja>
2016-04-13 20:48:27 -0700

    feat: Add burger button functionality.

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@
  */
 var locationData = [{
   name: "Dick's Drive-In",
-  filterTags: [undefined, "food", "burgers", "american"],
+  filterTags: ["", "food", "burgers", "american"],
   flickrQuery: "dick's drive in Seattle",
   yelpURL: "http://www.yelp.com/biz/dicks-drive-in-seattle-4",
   address: "500 Queen Anne Ave N, Seattle, WA 9810",
@@ -21,7 +21,7 @@ var locationData = [{
   long: -122.356383
 }, {
   name: "Thai Heaven",
-  filterTags: [undefined, "food", "thai"],
+  filterTags: ["", "food", "thai"],
   flickrQuery: "thai heaven Seattle",
   yelpURL: "http: //www.yelp.com/biz/thai-heaven-seattle",
   address: "352 Roy St, Seattle, WA 98109",
@@ -29,7 +29,7 @@ var locationData = [{
   long: -122.349916
 }, {
   name: "Roti Cuisine of India",
-  filterTags: [undefined, "food", "indian"],
+  filterTags: ["", "food", "indian"],
   flickrQuery: "Roti Seattle",
   yelpURL: "http: //www.yelp.com/biz/roti-indian-cuisine-seattle",
   address: "530 Queen Anne Ave N, Seattle, WA 98109",
@@ -37,7 +37,7 @@ var locationData = [{
   long: -122.356371
 }, {
   name: "Bahn Thai",
-  filterTags: [undefined, "food", "thai"],
+  filterTags: ["", "food", "thai"],
   flickrQuery: "bahn thai restaurant Seattle",
   yelpURL: "http: //www.yelp.com/biz/bahn-thai-restaurant-seattle",
   address: "409 Roy St, Seattle, WA 98109",
@@ -45,7 +45,7 @@ var locationData = [{
   long: -122.348381
 }, {
   name: "Bamboo Garden",
-  filterTags: [undefined, "food", "chinese", "vegetarian"],
+  filterTags: ["", "food", "chinese", "vegetarian"],
   flickrQuery: "bamboo garden restaurant seattle",
   yelpURL: "http: //www.yelp.com/biz/bamboo-garden-vegetarian-cuisine-seattle",
   address: "364 Roy St, Seattle, WA 98109",
@@ -53,7 +53,7 @@ var locationData = [{
   long: -122.349461
 }, {
   name: "Seattle Center",
-  filterTags: [undefined, "entertainment", "outdoor", "kid friendly"],
+  filterTags: ["", "entertainment", "outdoor", "kid friendly"],
   flickrQuery: "seattle center",
   yelpURL: "http: //www.yelp.com/biz/seattle-center-seattle",
   address: "305 Harrison St, Seattle, WA 98109",
@@ -61,7 +61,7 @@ var locationData = [{
   long: -122.351643
 }, {
   name: "SIFF Cinema Uptown",
-  filterTags: [undefined, "entertainment"],
+  filterTags: ["", "entertainment"],
   flickrQuery: "siff cinema uptown",
   yelpURL: "http: //www.yelp.com/biz/siff-cinema-uptown-seattle",
   address: "511 Queen Anne Ave N, Seattle, WA 98109",
@@ -69,7 +69,7 @@ var locationData = [{
   long: -122.356978
 }, {
   name: "Experience Music Project Museum",
-  filterTags: [undefined, "entertainment", "music"],
+  filterTags: ["", "entertainment", "music"],
   flickrQuery: "experience music project",
   yelpURL: "http: //www.yelp.com/biz/emp-museum-seattle",
   address: "325 5 th Ave N, Seattle, WA 98109",
@@ -77,7 +77,7 @@ var locationData = [{
   long: -122.348518
 }, {
   name: "McCaw Hall",
-  filterTags: [undefined, "entertainment", "performing arts", "art", "arts", "ballet"],
+  filterTags: ["", "entertainment", "performing arts", "art", "arts", "ballet"],
   flickrQuery: "mccaw hall seattle",
   yelpURL: "http: //www.yelp.com/biz/marion-oliver-mccaw-hall-seattle",
   address: "321 Mercer St, Seattle, WA 98109",
@@ -85,7 +85,7 @@ var locationData = [{
   long: -122.350088
 }, {
   name: "Seattle Children's Theater",
-  filterTags: [undefined, "entertainment", "performing arts", "art", "arts", "theater"],
+  filterTags: ["", "entertainment", "performing arts", "art", "arts", "theater"],
   flickrQuery: "seattle children 's theater",
   yelpURL: "http: //www.yelp.com/biz/seattle-childrens-theatre-seattle",
   address: "201 Thomas St, Seattle, WA 98109",
@@ -121,7 +121,7 @@ var Location = function(data) {
 var ViewModel = function() {
   var self = this;
   self.locations = ko.observableArray([]);
-  self.activeFilter = ko.observable(undefined);
+  self.activeFilter = ko.observable("");
   this.init = function() {
     locationData.forEach(function(location) {
       self.locations.push(new Location(location));
@@ -154,6 +154,8 @@ viewModel.init();
 ko.applyBindings(viewModel);
 
 var View = {
+  locElemPaneVisible: false,
+
   init: function() {
     this.initTypeahead();
   },
@@ -171,7 +173,19 @@ var View = {
     $('.typeahead.input-lg').siblings('input.tt-hint').addClass('hint-large');
   },
 
-  toggleLocElem: function() {
+  toggleLocElemPane: function() {
+    if(!this.locElemPaneVisible){
+      $("#loc-elem-pane").height($(window).height() - $("#filter-bar").height());
+      $("#loc-elem-pane").animate({top: 0 + $("#filter-bar").height()}, 1000);
+      this.locElemPaneVisible = true;
+    } else{
+      this.hideLocElemPane();
+    }
+  },
+
+  hideLocElemPane: function(){
+    $('#loc-elem-pane').animate({top: -1000}, 1000);
+    this.locElemPaneVisible = false;
   },
 
   favoriteLocation: function(id){
@@ -229,23 +243,34 @@ function initMap() {
       markers.forEach(function(marker){
         marker.setVisible(true);
       });
-      viewModel.activeFilter(undefined);
+      viewModel.activeFilter("");
       $("#clear-filter-btn").hide();
     });
     markers.push(marker);
     location.marker(marker);
   });
 
-  // Create a DOM listener that Goole maps can respond to.
-  google.maps.event.addDomListener($("#filter-btn").get(0), 'click', function() {
+  var runfilter = function(){
     var filter = $("#filter-box").val().toLowerCase();
 
     // If this is a valid filter tag
-    if($.inArray(filter, viewModel.getFilterTags()) != -1){
+    if($.inArray(filter, viewModel.getFilterTags()) != -1 && filter != ""){
       viewModel.activeFilter(filter);
       google.maps.event.trigger(marker, 'filter');
       $("#clear-filter-btn").show();
+
     }
+  }
+
+  google.maps.event.addDomListener($("#filter-box").get(0), 'keypress', function(e) {
+    if(e.which === 13){
+      runfilter();
+    }
+  });
+
+  // Create a DOM listener that Goole maps can respond to.
+  google.maps.event.addDomListener($("#filter-btn").get(0), 'click', function() {
+    runfilter();
   });
     google.maps.event.addDomListener($("#clear-filter-btn").get(0), 'click', function(){
     google.maps.event.trigger(marker, 'clear-filter');
